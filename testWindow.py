aeab8a5d3b41cd1fcdb15344af2e68843863ffca
juliencongras <julien.congras@gmail.com>
2024-02-18 15:56:07 +0100

    Font changing in progress

--- testWindow.py
+++ testWindow.py
@@ -2,12 +2,15 @@ import tkinter as tk, tkinter.font as tk_font
 from tkinter.ttk import Combobox
 from tkinter import *
 
+def new_font(font):
+    print(cb.info, font)
+
 windowWidth = 800
 windowHeight = 500
 
 window = tk.Tk()
 btn = Button(window, text="This is a button", bg="red", fg="blue")
-btn.place(x=windowWidth/2, y=windowHeight/2)
+btn.place(x=windowWidth / 2, y=windowHeight / 2)
 lbl = Label(window, text="This is a label with a great font.", fg="black", font=("Comic Sans MS", 16))
 lbl.place(x=50, y=50)
 txtfld = Entry(window, bg="white", fg="black")
@@ -16,6 +19,8 @@ txtfld.place(x=50, y=100)
 all_fonts = tk_font.families()
 cb = Combobox(window, values=all_fonts)
 cb.place(x=50, y=150)
+cb.bind("<FocusIn>", new_font)
+
 window.title("This is a test")
 
 # window.geometry("widthxheight+XPOS+YPOS") the size and position of the window
