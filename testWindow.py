d85f75a09a0cf6ee6c6c51b4c4ab79b648041925
juliencongras <julien.congras@gmail.com>
2024-02-18 12:48:21 +0100

    Tkinter window creation

---
+++ testWindow.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+# window.geometry("widthxheight+XPOS+YPOS") the size and position of the window
+
+window = tk.Tk()
+window.title("This is a test")
+window.geometry("800x500+10+10")
+window.mainloop()
