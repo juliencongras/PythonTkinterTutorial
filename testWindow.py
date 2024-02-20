94c5686eea1159d9bbfa18d135534c46218a3770
juliencongras <julien.congras@gmail.com>
2024-02-20 13:17:25 +0100

    Fonr changing works. Tutorial over.

--- testWindow.py
+++ testWindow.py
@@ -2,16 +2,20 @@ import tkinter as tk, tkinter.font as tk_font
 from tkinter.ttk import Combobox
 from tkinter import *
 
-def new_font(font):
-    print(cb.info, font)
+def new_font():
+    print(all_fonts[cb.current()])
+    fontName = str(all_fonts[cb.current()])
+    lbl.config(font=(fontName, fontSize))
 
 windowWidth = 800
 windowHeight = 500
+fontSize = 16
+fontName = "Comic Sans MS"
 
 window = tk.Tk()
 btn = Button(window, text="This is a button", bg="red", fg="blue")
 btn.place(x=windowWidth / 2, y=windowHeight / 2)
-lbl = Label(window, text="This is a label with a great font.", fg="black", font=("Comic Sans MS", 16))
+lbl = Label(window, text="This is a label with a great font.", fg="black", font=(fontName, fontSize))
 lbl.place(x=50, y=50)
 txtfld = Entry(window, bg="white", fg="black")
 txtfld.place(x=50, y=100)
